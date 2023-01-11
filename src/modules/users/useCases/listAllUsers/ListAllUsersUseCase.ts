823de114a1fc9b119478267afe92f939a34f144b
EdcleyHilario <edcleyhilario@gmail.com>
2023-01-11 15:08:19 -0300

    Finish-Implements-Solid

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -4,12 +4,18 @@ import { IUsersRepository } from "../../repositories/IUsersRepository";
 interface IRequest {
   user_id: string;
 }
-
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
-
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error(`User not exist: ${user_id}`).name;
+    }
+    if (!user.admin) {
+      throw new Error(`User ${user.email} not admin`).name;
+    }
+    const listAllUsers = this.usersRepository.list();
+    return listAllUsers;
   }
 }
 
